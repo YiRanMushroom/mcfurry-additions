b98deffec7e814cb735e06c3051366af5ab1cf60
Yiran Wang <114495322+YiRanMushroom@users.noreply.github.com>
2025-07-30 08:42:17 +0800

    Initial commit

---
+++ src/client/kotlin/com/github/yiranmushroom/MCFurryAdditionsClient.kt
@@ -0,0 +1,9 @@
+package com.github.yiranmushroom
+
+import net.fabricmc.api.ClientModInitializer
+
+object MCFurryAdditionsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
